d72c02c93c051faed308c39142679c162c781379
GatoPingado01 <150956945+GatoPingado01@users.noreply.github.com>
2025-08-25 13:46:35 -0300

    ajustando .gitignore e editando IndexPage

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,13 +1,26 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    />
+    
+    <div class="q-gutter-md" style="max-width: 300px">
+
+      <q-input filled v-model="email" type = "email" label="Email" />
+      <q-input filled v-model="password" type = "password" label="Senha" />
+      <q-btn push color="primary" label="Entrar" />
+
+    </div>
+
   </q-page>
 </template>
 
-<script setup>
-//
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup() {
+    return {
+      text: ref('')
+    }
+  }
+}
+
 </script>
